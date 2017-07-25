a84241b02938aff99855e471b803b9336c00faef
Bryan Laptop <bryanwillis7@gmail.com>
2017-07-25 15:04:46 -0400

    The script works fairly well. It searches the sm file and looks for the doubles section, and from there will search for specific song difficulties and write the notes into an array. The notes are packed into the array by measure, with each measure being written as a string. From here the measures can be split and further parsed. I think I'll go ahead and just develop this tool to export the SM files asjavascript objects that work in the level format that I've already written. I'll be going over my previous code soon to see what this looks like, this will also require me to make a little more effort and add an extra step that converts the notes to coordinate positions and times. Development of this tool will go on a little longer than previously anticipated, but as such it will knock out a huge portion of the overall game's development, and pave the way for the game's future and longevity.

--- smReader.js
+++ smReader.js
@@ -3,7 +3,13 @@
 //globals
 var fs = require('fs');
 var filePath = process.argv[2];
-var fileData, splitData 
+var noteSection = false;
+var doubleSection = false;
+var beginnerSong = false;
+var beginnerNotes = [];
+var mediumNotes = [];
+var fileData, splitData
+ 
 
 
 //First take in Data from the command line
@@ -14,5 +20,47 @@ fileData = fs.readFileSync(filePath, 'utf8');
 splitData = fileData.split(" ");
 console.log(splitData);
 
+var arrayLength = splitData.length;
+
+for(var i=0; i<arrayLength; i++){
+  if(splitData[i]== '----------------\r\n#NOTES:\r\n'){
+    console.log("Notes section found...");
+    noteSection = true;   
+  }
+  else if(splitData[i]=='dance-double:\r\n' && noteSection==true){
+    console.log("Doubles section found...");
+    doubleSection = true;
+  }
+  else if(splitData[i]=='Beginner:\r\n' && doubleSection==true){
+    beginnerSong = true;
+    mediumSong = false;  
+  }
+  else if(splitData[i]=='Medium:\r\n'){
+    mediumSong = true;
+    beginnerSong = false;
+  }
+  else if(splitData[i]=='measure' && noteSection==true && doubleSection==true)  {
+    var j = i+1;
+    if(beginnerSong==true){
+      beginnerNotes.push(splitData[j]);
+    }
+    else if(mediumSong==true){
+      mediumNotes.push(splitData[j]);
+    }
+  }
+  else if(splitData[i]=='dance-single:\r\n'){
+    noteSection = false;
+    doubleSection = false;
+    beginnerSong = false;
+    mediumSong = false;
+  }
+}
+
+console.log(beginnerNotes);
+
+//Error Messages go here
+if(noteSection=false){
+  console.log("Notes section not found!");
+}
 
 //Write then data to a new file
